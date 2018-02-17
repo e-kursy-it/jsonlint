61d0f5954b8769f32a171b3c4e2b6e0b16b55aad
Marek Bedkowski <marek@bedkowski.pl>
2018-02-17 17:20:27 +0100

    ES6'ify

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-var source = "var jsonlint = (function(){var require=true,module=false;var exports={};" +
+var source = "export var jsonlint = (function(){var require=true,module=false;var exports={};" +
   fs.readFileSync(__dirname+'/../lib/jsonlint.js', 'utf8') +
   "return exports;})()";
 
